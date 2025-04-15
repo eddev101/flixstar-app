324d31c975cb5554a155879051e1f6e8e31886c2
eddev101 <128457558+eddev101@users.noreply.github.com>
2025-04-15 16:53:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,7 @@
-const CACHE_NAME = 'flixstar-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/assets/css/main.css',
-    '/assets/css/style.css',
-    '/js/load-home-components.js',  // Added for index.html
-    '/js/load-components.js',      // For other pages
-    '/images/logo3.png'
-];
-
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+    console.log('Service Worker installed');
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+    console.log('Fetch event:', event.request.url);
 });
