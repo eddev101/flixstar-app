6f4d07f2c2813f6196825b7c513c4efb02c97f35
eddev101 <128457558+eddev101@users.noreply.github.com>
2026-03-01 16:37:22 +0200

    Update tvshows.js

--- js/tvshows.js
+++ js/tvshows.js
@@ -1,36 +1,49 @@
 const API_KEY = '5ec279387e9aa9488ef4d00b22acc451';
 let page = 1;
+let currentProvider = 'all';  // default: no filter (trending)
+const REGION = 'US';  // change to 'TR' if preferred; 'US' usually has more results
 
-function popularShows() {
-    axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=${API_KEY}&language=en-US&page=${page}`)
+function loadShows() {
+    let url = `https://api.themoviedb.org/3/trending/tv/week?api_key=${API_KEY}&language=en-US&page=${page}`;
+
+    if (currentProvider !== 'all') {
+        // Use discover with provider filter
+        url = `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}` +
+              `&language=en-US` +
+              `&sort_by=popularity.desc` +
+              `&page=${page}` +
+              `&watch_region=${REGION}` +
+              `&with_watch_providers=${currentProvider}`;
+    }
+
+    axios.get(url)
         .then(response => {
             const series = response.data.results;
             let output = '';
 
-            series.forEach(series => {
-                let poster = series.poster_path ? `https://image.tmdb.org/t/p/w342${series.poster_path}` : '../images/default.webp';
-                let year = series.first_air_date ? series.first_air_date.slice(0, 4) : 'N/A';
-
+            series.forEach(show => {
+                let poster = show.poster_path ? `https://image.tmdb.org/t/p/w342${show.poster_path}` : '../images/default.webp';
+                let year = show.first_air_date ? show.first_air_date.slice(0, 4) : 'N/A';
                 output += `
                     <a href="#" onclick="return false;">
                         <div class="movie-card">
                             <div class="card-head">
                                 <img src="${poster}" class="card-img">
                                 <div class="card-overlay">
-                                    <div class="bookmark" onclick="addToWatchlist(${series.id}, 'tv');">
+                                    <div class="bookmark" onclick="addToWatchlist(${show.id}, 'tv');">
                                         <ion-icon name="bookmark-outline"></ion-icon>
                                     </div>
                                     <div class="rating">
                                         <ion-icon name="star-outline"></ion-icon>
-                                        <span>${series.vote_average.toFixed(1)}</span>
+                                        <span>${show.vote_average.toFixed(1)}</span>
                                     </div>
-                                    <div class="play" onclick="viewDetails(${series.id}, 'tv');">
+                                    <div class="play" onclick="viewDetails(${show.id}, 'tv');">
                                         <ion-icon name="play-circle-outline"></ion-icon>
                                     </div>
                                 </div>
                             </div>
                             <div class="card-body">
-                                <h3 class="card-title">${series.name}</h3>
+                                <h3 class="card-title">${show.name}</h3>
                                 <div class="card-info">
                                     <span class="genre">Tv</span>
                                     <span class="year">${year}</span>
@@ -41,18 +54,38 @@ function popularShows() {
             });
 
             $('#shows').append(output);
-            page++;
 
-            if (page > response.data.total_pages) {
+            // Hide load more if no more pages
+            if (page >= response.data.total_pages) {
                 $('#loadMoreButton').hide();
+            } else {
+                $('#loadMoreButton').show();
             }
         })
-        .catch(error => console.log(error));
+        .catch(error => {
+            console.log(error);
+            $('#shows').append('<p style="color:red; text-align:center;">Failed to load shows</p>');
+        });
 }
 
 $(document).ready(() => {
-    popularShows();
-    $('#loadMoreButton').click(() => popularShows());
+    // Initial load
+    loadShows();
+
+    // Load more button
+    $('#loadMoreButton').click(() => {
+        page++;
+        loadShows();
+    });
+
+    // Filter dropdown change
+    $('#providerFilter').change(function() {
+        currentProvider = $(this).val();
+        page = 1;
+        $('#shows').empty();  // clear current grid
+        $('#loadMoreButton').show();
+        loadShows();
+    });
 });
 
 function addToWatchlist(id, type = 'tv') {
@@ -67,8 +100,6 @@ function addToWatchlist(id, type = 'tv') {
     }
 }
 
-
 function viewDetails(id, type) {
     window.location.href = `../tvshow/tvshow-details.html?id=${id}`;
 }
-
